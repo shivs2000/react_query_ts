dfe06a97c18af0a1c35dcf86c3eaffabc60fde41
shivs2000 <singhshivam0666@gmail.com>
2022-09-09 17:12:23 +0530

    added queryClientProvider,queried data

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,24 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Planets from "./components/Planets";
 import People from "./components/People";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 function App() {
   const [page, setPage] = useState("planets");
   return (
-    <div className="App">
-      <h1>Star wars App</h1>
-      <Navbar setPage={setPage} />
-      <div className="content ">
-        {page === "planets" ? <Planets /> : <People />}
+    // Provide the client to your App
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Star wars App</h1>
+        <Navbar setPage={setPage} />
+        <div className="content ">
+          {page === "planets" ? <Planets /> : <People />}
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
--- src/components/Planets.tsx
+++ src/components/Planets.tsx
@@ -1,6 +1,12 @@
 import React from "react";
-
+import { useQuery } from "@tanstack/react-query";
 const Planets = () => {
+  const fetchPlanets = async () => {
+    const res = await fetch(`https://swapi.dev/api/`);
+    return res.json();
+  };
+  const { data, status } = useQuery(["planets"], fetchPlanets);
+  console.log(data);
   return (
     <div>
       <h2>Planets</h2>

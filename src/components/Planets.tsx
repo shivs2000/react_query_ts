56f4e769f00eef38557d56d95bd24f4eb0952187
shivs2000 <singhshivam0666@gmail.com>
2022-09-10 22:39:51 +0530

    added planet component

--- src/components/Planets.tsx
+++ src/components/Planets.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
+import Planet from "./Planet";
 const Planets = () => {
   const fetchPlanets = async () => {
-    const res = await fetch(`https://swapi.dev/api/`);
+    const res = await fetch(`https://swapi.dev/api/planets/1/`);
     return res.json();
   };
   const { data, status } = useQuery(["planets"], fetchPlanets);
@@ -10,6 +11,20 @@ const Planets = () => {
   return (
     <div>
       <h2>Planets</h2>
+      <p>{status}</p>
+      {status === "error" && <div> Oops Error Occured</div>}
+      {status === "loading" && <div>Loding Wait!!</div>}
+      {status === "success" && (
+        <div>
+          {data.results.map((planet: any) => (
+            <Planet
+              name={planet.name}
+              terrain={planet.terrain}
+              population={planet.population}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
